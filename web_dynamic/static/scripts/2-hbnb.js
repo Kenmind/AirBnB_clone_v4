63bdc579abd9db31547472e9af3b2a046adfb89e
Kenmind <kennedykalaluka04@gmail.com>
2022-04-25 14:30:48 +0200

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,22 @@
+$('document').ready(function () {
+  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
+  $.get(url, function (response) {
+	    if (response.status === 'OK') {
+		    $('DIV#api_status').addClass('available');
+	    } else {
+		    $('DIV#api_status').removeClass('available');
+	    }
+  });
+
+  const amenities = {};
+
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+	    amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+	    delete amenities[$(this).attr('data-id')];
+    }
+
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+});
